cc6dfe067448325150b8e462ff6225620bc78c9c
vetraxdeano-wq <vetraxdeano@gmail.com>
2026-03-08 13:29:25 +0100

    Update supabase-init.js

--- js/supabase-init.js
+++ js/supabase-init.js
@@ -1,6 +1,8 @@
-const { createClient } = window.supabase
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
 
 const db = createClient(
   'https://ndsnbvvcbkblkncvoroc.supabase.co',
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5kc25idnZjYmtibGtuY3Zvcm9jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI5NjczMTksImV4cCI6MjA4ODU0MzMxOX0.mMsaBIrDI3BIoeTFBsCXo2AbuBLRnzqk625fKRkYCtM'
 )
+
+window.db = db
